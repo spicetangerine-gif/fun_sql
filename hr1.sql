e4627561580ef1b20b0c31b8c9cb9f4824834651
funfun <fun@yedam.ac>
2025-12-29 17:36:29 +0900

    1229

---
+++ hr1.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM tab;
+
+SELECT *
+FROM employees
+--where job_id = 'IT_PROG'
+where employee_id = 103;
+
+select *
+from jobs; -- IT_PROG
+
+select *
+from departments;
---
+++ yedam_sql2.sql
@@ -0,0 +1,107 @@
+--
+-- Copyright (c) Oracle Corporation 1988, 1999.  All Rights Reserved.
+--
+--  NAME
+--    demobld.sql
+--
+-- DESCRIPTION
+--   This script creates the SQL*Plus demonstration tables in the
+--   current schema.  It should be STARTed by each user wishing to
+--   access the tables.  To remove the tables use the demodrop.sql
+--   script.
+--
+--  USAGE
+--       SQL> START demobld.sql
+--
+--
+
+DROP TABLE SALGRADE;
+DROP TABLE BONUS;
+DROP TABLE EMP;
+DROP TABLE DEPT;
+
+CREATE TABLE DEPT
+       (DEPTNO NUMBER(2) primary key,
+        DNAME VARCHAR2(14),
+        LOC VARCHAR2(13) );
+
+INSERT INTO DEPT VALUES (10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO DEPT VALUES (20, 'RESEARCH',   'DALLAS');
+INSERT INTO DEPT VALUES (30, 'SALES',      'CHICAGO');
+INSERT INTO DEPT VALUES (40, 'OPERATIONS', 'BOSTON');
+
+CREATE TABLE EMP
+       (EMPNO NUMBER(4) PRIMARY KEY,
+        ENAME VARCHAR2(10) NOT NULL,
+        JOB VARCHAR2(9),
+        MGR NUMBER(4) REFERENCES emp(empno),
+        HIREDATE DATE,
+        SAL NUMBER(7, 2),
+        COMM NUMBER(7, 2),
+        DEPTNO NUMBER(2) REFERENCES dept(deptno));
+
+INSERT INTO EMP VALUES
+        (7839, 'KING',   'PRESIDENT', NULL,
+        TO_DATE('81/11/17', 'RR/MM/DD'), 5000, NULL, 10);
+INSERT INTO EMP VALUES
+        (7566, 'JONES',  'MANAGER',   7839,
+        TO_DATE('81/04/02', 'RR/MM/DD'),  2975, NULL, 20);
+INSERT INTO EMP VALUES
+        (7698, 'BLAKE',  'MANAGER',   7839,
+        TO_DATE('81/05/01', 'RR/MM/DD'),  2850, NULL, 30);
+INSERT INTO EMP VALUES
+        (7782, 'CLARK',  'MANAGER',   7839,
+        TO_DATE('81/06/09', 'RR/MM/DD'),  2450, NULL, 10);
+INSERT INTO EMP VALUES
+        (7902, 'FORD',   'ANALYST',   7566,
+        TO_DATE('81/12/03', 'RR/MM/DD'),  3000, NULL, 20);
+INSERT INTO EMP VALUES
+        (7788, 'SCOTT',  'ANALYST',   7566,
+        TO_DATE('82/12/09', 'RR/MM/DD'), 3000, NULL, 20);
+INSERT INTO EMP VALUES
+        (7499, 'ALLEN',  'SALESMAN',  7698,
+        TO_DATE('81/02/20', 'RR/MM/DD'), 1600,  300, 30);
+INSERT INTO EMP VALUES
+        (7521, 'WARD',   'SALESMAN',  7698,
+        TO_DATE('81/02/22', 'RR/MM/DD'), 1250,  500, 30);
+INSERT INTO EMP VALUES
+        (7654, 'MARTIN', 'SALESMAN',  7698,
+        TO_DATE('81/09/28', 'RR/MM/DD'), 1250, 1400, 30);
+INSERT INTO EMP VALUES
+        (7844, 'TURNER', 'SALESMAN',  7698,
+        TO_DATE('81/09/08', 'RR/MM/DD'),  1500,    0, 30);
+INSERT INTO EMP VALUES
+        (7900, 'JAMES',  'CLERK',     7698,
+        TO_DATE('81/12/03', 'RR/MM/DD'),   950, NULL, 30);
+INSERT INTO EMP VALUES
+        (7934, 'MILLER', 'CLERK',     7782,
+        TO_DATE('82/01/23', 'RR/MM/DD'), 1300, NULL, 10);
+INSERT INTO EMP VALUES
+        (7369, 'SMITH',  'CLERK',     7902,
+        TO_DATE('80/12/17', 'RR/MM/DD'),  800, NULL, 20);
+INSERT INTO EMP VALUES
+        (7876, 'ADAMS',  'CLERK',     7788,
+        TO_DATE('83/01/12', 'RR/MM/DD'), 1100, NULL, 20);
+
+CREATE TABLE BONUS
+        (ENAME VARCHAR2(10),
+         JOB   VARCHAR2(9),
+         SAL   NUMBER,
+         COMM  NUMBER);
+
+CREATE TABLE SALGRADE
+        (GRADE NUMBER,
+         LOSAL NUMBER,
+         HISAL NUMBER);
+
+INSERT INTO SALGRADE VALUES (1,  700, 1200);
+INSERT INTO SALGRADE VALUES (2, 1201, 1400);
+INSERT INTO SALGRADE VALUES (3, 1401, 2000);
+INSERT INTO SALGRADE VALUES (4, 2001, 3000);
+INSERT INTO SALGRADE VALUES (5, 3001, 9999);
+
+COMMIT;
+purge recyclebin ;
+SET TERMOUT ON
+PROMPT Demonstration table build is complete.
+
